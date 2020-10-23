64f5b4e8e68687fc2c4dc7e1c2fb16928117f076
Paytongray18 <paytongray18@gmail.com>
2020-10-22 22:58:26 -0500

    css almost done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -181,15 +181,30 @@ function createFormData() {
       const divHome = document.createElement('div')
       divHome.className = "divHome"
       const h1 = document.createElement('h1')
-      h1.innerText = "Welcome!"
-      h1.style = "color: goldenrod;"
-      h1.style.fontFamily = "arial"
+      const byline = document.getElementById('byline');     // Find the H2
+      bylineText = byline.innerHTML;                                      // Get the content of the H2
+      bylineArr = bylineText.split('');                                   // Split content into array
+      byline.innerHTML = '';                                                      // Empty current content
+
+      var span;                   // Create variables to create elements
+      var letter;
+
+      for(i=0;i<bylineArr.length;i++){                                    // Loop for every letter
+      span = document.createElement("span");                    // Create a <span> element
+      letter = document.createTextNode(bylineArr[i]);   // Create the letter
+      if(bylineArr[i] == ' ') {                                             // If the letter is a space...
+      byline.appendChild(letter);                 // ...Add the space without a span
+      } else {
+      span.appendChild(letter);                       // Add the letter to the span
+      byline.appendChild(span);                   // Add the span to the h2
+  }
+}
       
       
     
       divHome.append(h1)
       cardIndex.append(divHome)
-      document.querySelector(".star-wars").style.display = "block" 
+      document.querySelector(".star-wars").style.display = "none" 
     })
   
   }
